0fd0f80f04c527b4400fcb890ec1c850c42395be
JEYAWIN GJ <gjeyawin@gmail.com>
2026-02-26 18:39:07 +0530

    Java File

---
+++ 645. Set Mismatch.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int n = nums.length;
+        boolean[] seen = new boolean[n + 1];
+        int duplicate = 0;
+        
+        // Find the duplicate
+        for (int num : nums) {
+            if (seen[num]) {
+                duplicate = num;
+            } else {
+                seen[num] = true;
+            }
+        }
+        for (int i = 1; i <= n; i++) {
+            if (!seen[i]) {
+                return new int[]{duplicate, i};
+            }
+        }
+        
+        return new int[]{};
+    }
+}
